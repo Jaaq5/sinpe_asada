0275e9878f71380c6503afee440004ad619267cf
Alex Artavia <joseaaq5@gmail.com>
2024-06-17 13:22:02 -0600

    Add page header

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,11 +1,13 @@
 // app/ui/Header.tsx
 import React from "react";
 import NavBar from "./nav-bar";
+import { HeaderTabs } from "../components/mantine-nav-bar";
 
 const Header: React.FC = () => {
   return (
     <header>
-      <NavBar /> {/* Incluye el NavBar aquí */}
+      <HeaderTabs /> {/* Incluye el NavBar aquí */}
+      <br />
       {/* Aquí puedes añadir otros elementos del encabezado si los necesitas */}
     </header>
   );
