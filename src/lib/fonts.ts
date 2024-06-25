0ed783f2db63cdd620d5c11d70526347ef0be02c
Alex Artavia <joseaaq5@gmail.com>
2024-06-25 17:31:37 -0600

    Add Roboto and Monserrat fonts

---
+++ src/lib/fonts.ts
@@ -0,0 +1,14 @@
+import { Roboto } from "next/font/google";
+import { Montserrat } from "next/font/google";
+import { Edu_QLD_Beginner } from "next/font/google";
+
+export const roboto = Roboto({
+  subsets: ["latin"],
+  weight: "400",
+});
+export const monserrat = Montserrat({
+  subsets: ["latin"],
+});
+export const eduQldBeginner = Edu_QLD_Beginner({
+  subsets: ["latin"],
+});
