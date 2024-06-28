457b68361386c9f0eb9a99466b6e18e6f248a928
Alex Artavia <joseaaq5@gmail.com>
2024-06-27 22:09:53 -0600

    Remove background video

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,13 @@ import Header from "../components/header";
 
 export default function Home() {
   return (
-    <div className="relative min-h-screen bg-white">
+    <div className="relative min-h-screen">
       <br />
       <div className="absolute inset-0 overflow-hidden">
         {/* Capa semitransparente */}
         <div className="absolute inset-0 bg-black opacity-10"></div>
         {/* Video de fondo */}
+        {/*
         <iframe
           className="absolute top-0 left-0 w-full h-full object-cover pointer-events-none"
           src="https://www.youtube.com/embed/rEe9DNmuqVw?autoplay=1&mute=1&controls=0&playlist=rEe9DNmuqVw&loop=1"
@@ -17,6 +18,7 @@ export default function Home() {
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen
         ></iframe>
+        */}
       </div>
       <main className="relative z-10 mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 lg:mt-16 lg:px-8">
         <div className="text-center text-white">
