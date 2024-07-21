9d0161655c5a05100f41edc5cfd5cd4ae4b60844
Alex Artavia <joseaaq5@gmail.com>
2024-07-20 22:37:11 -0600

    Remove daisyui library

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,5 @@
 import type { Config } from "tailwindcss";
-import daisyui from "daisyui";
+//import daisyui from "daisyui";
 
 const config: Config = {
   content: [
@@ -16,7 +16,7 @@ const config: Config = {
       },
     },
   },
-  plugins: [daisyui],
+  //plugins: [daisyui],
 };
 
 export default config;
